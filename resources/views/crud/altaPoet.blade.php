5a60d0858839898c9fbb6dcf679b89d1a9a4a6fd
MarinaSAG <colorvp3@gmail.com>
2020-12-13 20:31:25 -0700

    users a poets

--- resources/views/crud/altaPoet.blade.php
+++ resources/views/crud/altaPoet.blade.php
@@ -1,45 +1,61 @@
 @extends('layouts.master')
-@if(isset($user) and is_object($user))
+@if(isset($poet) and is_object($poet))
     @php
-        $title = 'Modificación de un Usuario';
+        $title = 'Modificación de un Registro';
         $boton = 'Actualizar';
-        $id = $user->idUsuario;
-        $Usuario = $user->Usuario;
-        $password = $user->password;
-        $key = $user->key;
+        $poet_code = $poet->poet_code;
+        $first_name = $poet->first_name;
+        $surname = $poet->surname;
+        $adress = $poet->adress;
+        $postcode = $poet->postcode;
+        $telephone_number = $poet->telephone_number;
     @endphp
 @else
     @php
-        $title='Alta de Usuario';
+        $title='Alta de un Registro';
         $boton = 'Dar de alta';
-        $id = '';
-        $Usuario = '';
-        $password = '';
-        $key = '';
+        $poet_code = '';
+        $first_name = '';
+        $surname = '';
+        $adress = '';
+        $postcode = '';
+        $telephone_number = '';
     @endphp
 @endif
 
-@section('title', 'Alta de Usuario')
+@section('title', 'Alta de Registro')
 @section('header')
 @section('content')
-    Alta de usuario
+    Alta de registro
     <center>
-        <form action="{{isset($user) ? action('Crud@update') :action('Crud@store')}}" method="POST">
+        <form action="{{isset($poet) ? action('Poets@update') :action('Poets@store')}}" method="POST">
             {{csrf_field()}}
-            @if(isset($user) and is_object($user))
-                <input type="hidden" name="id" value="{{$id}}">
+            @if(isset($poet) and is_object($poet))
+                <input type="hidden" name="id" value="{{$poet_code}}">
             @endif
-            <label for="user">Usuario</label>
+            <label for="poet_code">Poet code</label>
             <br>
-            <input type="text" name="user" value="{{$Usuario}}">
+            <input type="text" name="poet_code" value="{{$poet_code}}">
             <br>
-            <label for="password">Password</label>
+            <label for="first_name">First name</label>
             <br>
-            <input type="password" name="password" value="{{$password}}">
+            <input type="text" name="first_name" value="{{$first_name}}">
             <br>
-            <label for="key">Key</label>
+            <label for="surname">Surname</label>
             <br>
-            <input type="text" name="key" value="{{$key}}">
+            <input type="text" name="surname" value="{{$surname}}">
+            <br>
+            <label for="adress">Adress</label>
+            <br>
+            <input type="text" name="adress" value="{{$adress}}">
+            <br>
+            <label for="postcode">Postcode</label>
+            <br>
+            <input type="text" name="postcode" value="{{$postcode}}">
+            <br>
+            <label for="telephone_number">Telephone number</label>
+            <br>
+            <input type="text" name="telephone_number" value="{{$telephone_number}}">
             <br><br>
             <input type="submit" name="submit" value="{{$boton}}">
         </form>
